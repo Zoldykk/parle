8b656f49548cb8c753066df359ceef4cfe9662b6
Zoldykk <zakariaatattou4@outlook.com>
2021-03-23 20:23:33 +0100

    registration validation implemented

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,22 +1,67 @@
-import {Link} from 'react-router-dom'
+import { useState } from 'react'
+import {Link, useHistory } from 'react-router-dom'
 import '../styles/Register.css'
 
 export default function Register() {
+    let history = useHistory();
+    const [username, setUsername] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const [usernameError, setUsernameError] = useState('')
+    const [emailError, setEmailError] = useState('')
+    const [passwordError, setPasswordError] = useState('')
+
+    const handleSubmit = async (e) =>{
+        e.preventDefault()
+        try{
+            const response = await fetch('http://localhost:5000/register', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    username,
+                    email,
+                    password
+                })
+            })
+            const data = await response.json()
+            if(data.errors){
+                setUsernameError(data.errors.username)
+                setEmailError(data.errors.email)
+                setPasswordError(data.errors.password)
+            }else{
+                history.push('/')
+            }
+        }
+        catch(err){
+            console.log(err)
+        }
+    }
+
     return (
         <div className='container'>
             <div className="logo">
                 <img src="/logo.png" alt=""/>
             </div>
             <div className="form">
-                <form>
-                    <input type="text" placeholder='Username'/>
-                    <div className='username-error'></div>
-                    <input type="text" placeholder='Email'/>
-                    <div className='email-error'></div>
-                    <input type="password" placeholder='Password'/>
-                    <div className='password-error'></div>
-                    <input type="password" placeholder='Password verify'/>
-                    <div className='passwordVerify-error'></div>
+                <form onSubmit={handleSubmit}>
+                    <div className="username">
+                        <input onChange={(e)=> setUsername(e.target.value)} type="text" placeholder='Username'/>
+                        <div className='username-error'>{usernameError}</div>
+                    </div>
+
+                    <div className="email">
+                        <input onChange={(e)=> setEmail(e.target.value)} type="text" placeholder='Email'/>
+                        <div className='email-error'>{emailError}</div>
+                    </div>
+
+                    <div className="password">
+                        <input onChange={(e)=> setPassword(e.target.value)} type="password" placeholder='Password'/>
+                        <div className='password-error'>{passwordError}</div>
+                    </div>
+
                     <button className='register-btn' type='submit'>Register</button>
                     <hr/>
                     <Link to='/' className='login-btn'>Login</Link>
